7cb4bca7aa0b9f29d0050deb35991deabb96eaec
Joel  Arias Cordova <146593148+joelariasc79@users.noreply.github.com>
2025-08-10 19:36:39 -0500

    Add Assessment_06_MEAN Bank End Endpoints

--- Assessments/Assessment_05_React_Vaccination_System/CareShieldHealthCareSystem/vaccination-system-react/src/store/features/reports/reportsSelectors.js
+++
@@ -1,8 +0,0 @@
-// src/store/features/reports/reportsSelectors.js
-
-export const selectUserDemographicsReport = (state) => state.reports.userDemographics;
-export const selectDosesDailyReport = (state) => state.reports.dosesDaily;
-export const selectPopulationCoverageReport = (state) => state.reports.populationCoverage;
-export const selectReportsStatus = (state) => state.reports.status;
-export const selectReportsError = (state) => state.reports.error;
-export const selectWatchlistSummary = (state) => state.reports.watchlistSummary;
